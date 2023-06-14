f7215776ad5037b01f58124f99d84879554537b6
niiimonteiros <130519784+niiimonteiros@users.noreply.github.com>
2023-06-13 23:37:32 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,20 @@
+var mensagem = "Página Carregada com Sucesso!";
+alert(mensagem);
+
+var x = 8;
+var y = 6;
+
+var z = x + y;
+document.getElementById("soma").innerHTML = z;
+
+var z = x / y;
+document.getElementById("divisao").innerHTML = z;
+
+var z = x * y;
+document.getElementById("multi").innerHTML = z;
+
+var z =  x <= y;
+document.getElementById("Comparação").innerHTML = z;
+
+var z = ((x / y)/2) >= 2;
+document.getElementById("Comparação2").innerHTML = z;
